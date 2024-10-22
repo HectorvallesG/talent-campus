bf61ffdca93ded3e416a63d9b9c08ba3827fbe14
Oliver <oliver.kali.hub@gmail.com>
2024-10-21 18:13:39 -0600

    add: validations schema

--- src/app/(auth)/crear-cuenta/_components/RecluiterForm.tsx
+++ src/app/(auth)/crear-cuenta/_components/RecluiterForm.tsx
@@ -1,23 +1,28 @@
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { BasicAccountSchema, BasicAccountSchemaModel } from "@/modules/account/account.schema"
+import { RecruiterSchemaModel } from "@/modules/account/account.schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { RecruiterSchema } from '../../../../modules/account/account.schema';
 
 
 export const RecluiterForm = () => {
   
-  const form = useForm<BasicAccountSchemaModel>({
-    resolver: zodResolver(BasicAccountSchema),
+  const form = useForm<RecruiterSchemaModel>({
+    resolver: zodResolver(RecruiterSchema),
     defaultValues:{
       email: '',
       password: '',
+      name: '',
+      company: '',
+      tel: '',
+      userName: ''
     }
   })
 
 
-  const onSubmit = (data:BasicAccountSchemaModel) => {
+  const onSubmit = (data:RecruiterSchemaModel) => {
     console.log("llego");
     console.log(data);
   };
@@ -32,20 +37,76 @@ export const RecluiterForm = () => {
             <legend className="text-xl font-semibold border-b-2 pb-2 my-2 border-gray-200">
               Una cuenta para Reclutador
             </legend>
+         
           <FormField
             control={form.control}
-            name="email"
+            name="company"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Correo</FormLabel>
+                <FormLabel>Nombre de la organización</FormLabel>
                 <FormControl>
-                  <Input placeholder="example@example.com" {...field} />
+                  <Input placeholder="Organización" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre al que debemos dirigirnos</FormLabel>
+                <FormControl>
+                  <Input placeholder="Jhon" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="tel"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Número de contacto</FormLabel>
+                <FormControl>
+                  <Input placeholder="Tel" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
+          <div className="flex gap-2">
+             <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Correo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+             <FormField
+              control={form.control}
+              name="userName"
+              render={({ field }) => (
+                <FormItem className="w-1/2">
+                  <FormLabel>Nombre del usuario</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Jhon872" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+  
+          </div>
           <FormField
             control={form.control}
             name="password"
